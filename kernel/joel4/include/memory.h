b975df837e869e632eda770f454d00199f7e20dd
Joel Nider <joel@nider.org>
2012-10-07 15:54:56 +0200

    kernel: initialized memory enough to enable paging
    Set up the kernel's page directory, and page table 0.  Mapped the
    first 4 MB of memory 1:1, and enabled paging.

--- kernel/joel4/include/memory.h
+++ kernel/joel4/include/memory.h
@@ -5,7 +5,9 @@
 #define PAGE_SIZE_MASK        (PAGE_SIZE-1)  ///< number of bits in page size
 
 typedef unsigned int PageTable;
-typedef unsigned int PageDirectory;
+typedef unsigned int PageDirectory[PAGE_SIZE/sizeof(unsigned int)];
+
+void k_initMemory(PageDirectory* pPageDir);
 
 #endif /* _MEMORY__H */
 
