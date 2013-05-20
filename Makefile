1d5526f98890b781bbf0ac84e2e4e4ae3a893406
jnider <joel@nider.org>
2013-05-20 20:35:57 +0300

    Added drivers sub-directory
    The first attempt at the Dominos drivers.  I started with the pcie bus,
    which is just a stub at this point.  The real goal was to build the make
    system, and test the loading function of the root task.
    
    Signed-off-by: jnider <joel@nider.org>

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 kerneldir=kernel/joel4
 
-all:
-	make -C $(kerneldir)
+all: build_all init.cpio
 	./makeiso.sh grub2
+
+build_all:
+	make -C $(kerneldir)
+	make -C drivers
 
 init.cpio: initfiles.txt boot.txt
 	cpio --format=newc -o < initfiles.txt > init.cpio
---
+++ drivers/tools.mk
@@ -0,0 +1,22 @@
+# tools.mk
+# 
+# contains executable names of all of the tools
+# to be used during the building of the project
+# such as compiler, assembler, linker, etc.
+#
+# jnider 17-08-2012
+
+CC=gcc
+LD=ld
+AS=as
+CTAGS=ctags
+
+CFLAGS+=-fno-stack-protector # stack protector functions are not implemented yet
+CFLAGS+=-nostdinc # don't include paths other than what I tell you
+CFLAGS+=-ffreestanding # ignore built-in functions, stdlib, etc
+CFLAGS+=-g #add debugging info
+CFLAGS+=-m32 #compile for 32-bit target
+
+ASMFLAGS= --32
+
+LDFLAGS=-melf_i386
