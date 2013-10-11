56be942fcd1d48dc7730b3144be9a4b8920491be
Joel Nider <joel@nider.org>
2013-10-11 10:47:27 +0200

    JOEL4: don't delete .o files when building
    MAKE has this horrible habit of cleaning up things it thinks I don't need.
    Now I have to tell make to stop deleting .o files, because I don't want to
    have to compile them every time.
    
    Signed-off-by: Joel Nider <joel@nider.org>

--- kernel/joel4/rules.mk
+++ kernel/joel4/rules.mk
@@ -8,6 +8,9 @@ C_OBJS=$(C_SRC:%.c=objs/%.o)
 S_OBJS=$(S_SRC:%.s=objs/%.o)
 OBJS=$(S_OBJS) $(C_OBJS)
 
+# don't delete my .o files!
+.PRECIOUS: objs/%.o
+
 objs/%.o: src/%.s
 	@echo "Assembling $<"
 	@$(AS) $(ASMFLAGS) $(INCLUDE_PATHS) -c $< -o $@
