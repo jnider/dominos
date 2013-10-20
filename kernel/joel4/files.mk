9ea54a225c11382d7cdca077e6596d9c488b7cbf
Joel Nider <joel@nider.org>
2013-10-20 13:26:41 +0200

    JOEL4: added some makefile documentation
    Documenting the use of files.mk variables a bit better.  There is still a lot
    more work to be done in this area.
    
    Signed-off-by: Joel Nider <joel@nider.org>

--- kernel/joel4/files.mk
+++ kernel/joel4/files.mk
@@ -3,6 +3,12 @@
 # list of files to be compiled
 #
 # jnider 17-08-2012
+#
+# Defines a set of variables that the main makefile can use
+# The variables defined in this file, but used elsewhere are:
+#
+# C_SRC = list of all 'normal' C source files
+# S_SRC = list of all 'normal' assembly files
 
 INTEL?=y
 
@@ -25,5 +31,5 @@ ifeq ($(strip $(INTEL)), y)
    CFLAGS += -DARCH_IA32
 endif
 
-S_SRC = boot.s    \
+S_SRC =  boot.s    \
          isr_stub.s
