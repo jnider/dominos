68ce4e1edd14667e7bd42490303ae55c311d3cf9
Joel Nider <joel@nider.org>
2014-03-22 17:54:31 +0200

    added mem_manager service
    Acts as a physical page allocator for all userspace threads.
    
    Signed-off-by: Joel Nider <joel@nider.org>

--- user/services/files.mk
+++ user/services/files.mk
@@ -6,10 +6,18 @@
 
 OUTPUT_DIR=objs
 ROOT?=y
+MEM_MANAGER?=y
 
 ifeq ($(strip $(ROOT)), y)
-	root_SRC = root/root_task.c
+	root.SRC = root/root_task.c
 	root.OBJS = root/root_task.o
 	SERVICES += root
+endif
+
+ifeq ($(strip $(MEM_MANAGER)), y)
+	mem_manager.SRC = mem_manager/main.c
+	mem_manager.OBJS = mem_manager/main.o
+	# mem_manager.OBJS = $(mem_manager_SRC:%.c/%.o)
+	SERVICES += mem_manager
 endif
 
