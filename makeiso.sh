b314dbff14c494edc78f04c6aefc15a6a8f60aad
Joel Nider <joel@nider.org>
2018-05-16 17:23:04 +0300

    Get rid of anything that is not GRUB
    Signed-off-by: Joel Nider <joel@nider.org>

--- makeiso.sh
+++ makeiso.sh
@@ -1,77 +1,28 @@
 #!/bin/bash
 
-PS3='>'
-
-loader=$1
-if [[ ! $loader ]]; then
-echo "Select a boot loader:"
-select loader in grub grub2 syslinux ; do
-	break
-done
-fi
-
 # set up the files for an iso image
 ISOROOT=iso-temp
 mkdir -p $ISOROOT
 
-# put the files in the right locations
+# put the kernel and initrd in the right locations
 echo "Copying files for image"
 cp kernel/joel4/joel4.elf $ISOROOT
-cp user/drivers/*.driver $ISOROOT
-cp user/services/*.service $ISOROOT
 cp init.cpio $ISOROOT
 
-if [[ $loader == "grub" ]] ; then
-	echo "Using GRUB Legacy"
-	mkdir -p $ISOROOT/boot/grub
-	#cp image/grub.cfg $ISOROOT/boot/grub/grub.cfg
-	cp /usr/lib/grub/i386-pc/* $ISOROOT/boot/grub/
-	iso_options='-R -b boot/grub/boot.img'
-fi
+echo "Using GRUB 2"
 
-if [[ $loader == "grub2" ]] ; then
-	echo "Using GRUB 2"
-	mkdir -p $ISOROOT/boot/grub
-	rm $ISOROOT/boot/grub/*
-	cp image/grub2.cfg $ISOROOT/boot/grub/grub.cfg
-	grub-mkrescue -o dominos.iso $ISOROOT
+# make sure the correct tools are available - grub-mkrescue fails silently if xorriso is not installed
+xorriso=$(which xorriso)
+if [[ ! -x $xorriso ]]; then
+	echo "You must install xorriso for this to work"
 	exit
 fi
 
-if [[ $loader == "syslinux" ]] ; then
-echo "Using SYSLINUX"
-# find syslinux, which is the bootloader
-SYSLINUX=`which syslinux`
-SYSLINUXDIR=`echo $SYSLINUX | sed 's:/bin/:/share/:'`
-[ -d "$SYSLINUXDIR" ] || SYSLINUXDIR="`echo $SYSLINUXDIR | sed 's:/share/:/lib/:'`"
-[ -d "$SYSLINUXDIR" ] || {
-       echo >&2 "Cannot find syslinux data directory!"
-       exit 1
-}
-
-echo "installing ISOLINUX-specific config files"
-mkdir -p $ISOROOT/isolinux
-cp $SYSLINUXDIR/isolinux.bin $ISOROOT/isolinux
-cp image/syslinux.cfg $ISOROOT/isolinux
-cp $SYSLINUXDIR/mboot.c32 $ISOROOT/isolinux
-
-iso_options='-b isolinux/isolinux.bin -c isolinux/boot.cat '
+if [[ -d $ISOROOT/boot/grub ]]; then
+	rm -f $ISOROOT/boot/grub/*
+else
+	mkdir -p $ISOROOT/boot/grub
 fi
 
-# now make an ISO 8660 El Torito bootable CD image
-genisoimage -o dominos.iso -A dominos $iso_options -no-emul-boot -boot-load-size 4 -boot-info-table -iso-level 3 -l $ISOROOT
-
-# clean up
-#rm -rf $TEMPDIR
-
-
-# --- the old way ----
-# put a partition table (MBR) on our new disk
-#echo "Creating MBR"
-#parted -s $dev mklabel msdos
-
-# add a single bootable partition
-#echo "Adding bootable partition"
-#sfdisk $dev -u S -C 4 -H 255 -S 63 << EOF
-#63,64197,0xc,*
-#EOF
+cp image/grub2.cfg $ISOROOT/boot/grub/grub.cfg
+grub-mkrescue -o dominos.iso -- --eltorito-id JOEL $ISOROOT
