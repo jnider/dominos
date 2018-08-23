1002c9b243cdb0c002d6d7f87876d32e218c39c5
Joel Nider <joel@nider.org>
2018-08-24 01:29:51 +0300

    Experiment with boot & load addresses
    Signed-off-by: Joel Nider <joel@nider.org>

--- kernel/joel4/src/boot.s
+++ kernel/joel4/src/boot.s
@@ -2,18 +2,19 @@
 
 .global _entry_point           # making entry point visible to linker
 .extern _kernelStack
+.extern _boot_sys_start
 
 # setting up the Multiboot header - see GRUB docs for details
 .set ALIGN,    0<<0             # align loaded modules on page boundaries
 .set MEMINFO,  1<<1             # provide memory map
 .set VIDEO,    1<<2              # video mode table
-.set LOAD,     0<<16             # load addresses are available
+.set LOAD,     1<<16             # load addresses are available
 
 .set FLAGS,    ALIGN | MEMINFO  # this is the Multiboot 'flag' field
 .set MAGIC,    0x1BADB002       # 'magic number' lets bootloader find the header
 .set CHECKSUM, -(MAGIC + FLAGS) # checksum required
-.set HEADER_ADDR, 0 #_startKernel
-.set LOAD_ADDR, 0 #_startKernel
+.set HEADER_ADDR, 0x101000
+.set LOAD_ADDR, 0x100000
 .set LOAD_END_ADDR, 0
 .set BSS_END_ADDR, 0 #_ebss
 .set ENTRY_ADDR, _entry_point
